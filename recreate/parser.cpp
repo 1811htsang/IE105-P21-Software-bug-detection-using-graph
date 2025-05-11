97827132a633dd3a234e5cb3f2c4c326b8ffe478
1811htsang <macsin233@gmail.com>
2025-05-11 11:00:15 +0700

    add GNU C/C++ manual, update extract function with 2 method, forgot to remove .bc file and parser compiled file but may remove at later commit

--- recreate/parser.cpp
+++ recreate/parser.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <sstream>
+#include <cstdio>
 using namespace std;
 
 #define DEBUG true
@@ -35,8 +37,36 @@ public:
 vector<string> callgraph_tokens; // tokenize the callgraph, use to store the callgraph
 map<int, string> IDtoFunc; // to store the function ID to function name mapping f(ID) = name
 map<string, int> FunctoID; // to store the function name to function ID mapping f(name) = ID
-map<int, vector<int>> FuncCalls; // to store the function calls, f(ID) = vector of function IDs
+
+// global variables for basic total reduction
+map<int, vector<int>> FuncCallTotalReduction; // to store the function calls, f(ID) = vector of function IDs
 int maxID; // to store the maximum function ID
+
+//global variables for total reduction with edge weight
+map<int, map<int, int>> FuncCallTotalReductionWeight; // to store the function calls, f(ID) = vector of function IDs with edge weight
+int threshold; // to store the threshold for edge weight
+
+
+string demangle(const string& mangled_name) {
+    string command = "c++filt -t " + mangled_name;
+    FILE* pipe = popen(command.c_str(), "r");
+    if (!pipe) {
+        return mangled_name; // Trả lại tên gốc nếu popen thất bại
+    }
+    char buffer[128];
+    string result = "";
+    while (!feof(pipe)) {
+        if (fgets(buffer, 128, pipe) != NULL) {
+            result += buffer;
+        }
+    }
+    pclose(pipe);
+    // Loại bỏ newline nếu có
+    if (!result.empty() && result.back() == '\n') {
+        result.pop_back();
+    }
+    return result;
+}
 
 string retFunctionName(const string &token)
 {
@@ -48,7 +78,7 @@ string retFunctionName(const string &token)
 } 
 
 //*
-void extract()
+void extract_basic_total_reduction()
 {
     // initialize some parameters
     int ID = -1; // to store the function ID
@@ -68,6 +98,8 @@ void extract()
     // ID assignment to functions
     // Iterate through the callgraph tokens and retrieve all the functions that are being called
     // once retrieved assign them a unique ID to avoid doing string comparison
+    // split this to a function
+
     for (; i < callgraph_tokens.size(); i++)
     {
         // ignore empty lines
@@ -106,21 +138,105 @@ void extract()
             // check if the function ID is already in the FuncCalls map
             // if it is, check if the function ID is already in the vector
             // if it is not, add it to the vector
-            if (FuncCalls.find(FunctoID[TopLevelFunc]) != FuncCalls.end())
+            if (FuncCallTotalReduction.find(FunctoID[TopLevelFunc]) != FuncCallTotalReduction.end())
             {
                 // check if the function ID is already in the vector
                 // if it is not, add it to the vector
                 // else, add it to the vector
-                if (find(FuncCalls[FunctoID[TopLevelFunc]].begin(),
-                         FuncCalls[FunctoID[TopLevelFunc]].end(),
-                         FunctoID[func]) == FuncCalls[FunctoID[TopLevelFunc]].end())
+                if (find(FuncCallTotalReduction[FunctoID[TopLevelFunc]].begin(),
+                         FuncCallTotalReduction[FunctoID[TopLevelFunc]].end(),
+                         FunctoID[func]) == FuncCallTotalReduction[FunctoID[TopLevelFunc]].end())
                 {
-                    FuncCalls[FunctoID[TopLevelFunc]].push_back(FunctoID[func]);
+                    FuncCallTotalReduction[FunctoID[TopLevelFunc]].push_back(FunctoID[func]);
                 }
             }
             else
             {
-                FuncCalls[FunctoID[TopLevelFunc]].push_back(FunctoID[func]);
+                FuncCallTotalReduction[FunctoID[TopLevelFunc]].push_back(FunctoID[func]);
+            }
+        }
+    }
+    maxID = ID;
+}
+
+void extract_total_reduction_weight() {
+// initialize some parameters
+    int ID = -1; // to store the function ID
+    int i; // to iterate through the callgraph tokens
+    string func, TopLevelFunc = "";
+
+    // ignore unnecessary lines
+    for (i = 1; i < callgraph_tokens.size(); i++)
+    {
+        if (callgraph_tokens[i] == "")
+        {
+            i++;
+            break;
+        } //
+    } //
+
+    // ID assignment to functions
+    // Iterate through the callgraph tokens and retrieve all the functions that are being called
+    // once retrieved assign them a unique ID to avoid doing string comparison
+    // split this to a function
+
+    for (; i < callgraph_tokens.size(); i++)
+    {
+        // ignore empty lines
+        if (callgraph_tokens[i] == "")
+        {
+            continue;
+        }
+
+        // check if the line contains a function call
+        if (callgraph_tokens[i].find("function:") != string::npos ||
+            callgraph_tokens[i].find("function") != string::npos)
+        {
+
+            func = retFunctionName(callgraph_tokens[i]);
+            if (FunctoID.find(func) == FunctoID.end())
+            {
+                ID++;
+                FunctoID[func] = ID;
+                IDtoFunc[ID] = func;
+            } 
+        } 
+
+        // check if the line contains a function call
+        // if it does, assign the function name to TopLevelFunc
+        // else, assign the function name to func
+        // and add the function ID to the FuncCalls map
+        if (callgraph_tokens[i].find("function:") != string::npos)
+        {
+            TopLevelFunc = retFunctionName(callgraph_tokens[i]);
+        }
+        else if (callgraph_tokens[i].find("function") != string::npos)
+        {
+
+            func = retFunctionName(callgraph_tokens[i]);
+
+            // check if the function ID is already in the mapping 
+            // if it is, check if the function ID is already in the map
+                // if the function ID is already in the map
+                // count the number of times it appears and increment the edge weight
+            // if it is not, add it to the map and increment the edge weight, in which based edge weight is set to 1
+            
+            if (FuncCallTotalReductionWeight.find(FunctoID[TopLevelFunc]) != FuncCallTotalReductionWeight.end())
+            {
+                // check if the function ID is already in the map
+                // if it is, increment the edge weight
+                if (FuncCallTotalReductionWeight[FunctoID[TopLevelFunc]].find(FunctoID[func]) != FuncCallTotalReductionWeight[FunctoID[TopLevelFunc]].end())
+                {
+                    FuncCallTotalReductionWeight[FunctoID[TopLevelFunc]][FunctoID[func]]++;
+                }
+                else
+                {
+                    FuncCallTotalReductionWeight[FunctoID[TopLevelFunc]][FunctoID[func]] = 1;
+                }
+            }
+            else
+            {
+                FuncCallTotalReductionWeight[FunctoID[TopLevelFunc]][FunctoID[func]] = 1;
             }
         }
     }
@@ -141,14 +257,14 @@ void printFuntionMapping(map<int, string> &IDtoFunc)
     cout << "function map :" << endl;
     for (map<int, string>::iterator it = IDtoFunc.begin(); it != IDtoFunc.end(); ++it)
     {
-        cout << it->first << " -> " << it->second << '\n';
+        cout << it->first << " -> " << it->second << " == " << demangle(it->second) << '\n';
     }
 }
 
 void printCallFunctions(map<int, vector<int>> &FuncCalls)
 {
     cout << endl
-         << "call functions :" << endl;
+         << "call functions with total reduction no edge weight :" << endl;
     for (map<int, vector<int>>::iterator it = FuncCalls.begin(); it != FuncCalls.end(); ++it)
     {
         cout << it->first << " calls: ";
@@ -160,12 +276,29 @@ void printCallFunctions(map<int, vector<int>> &FuncCalls)
     }
 }
 
+void printCallFunctionsWeight(map<int, map<int, int>> &FuncCalls)
+{
+    cout << endl
+         << "call functions with edge weight :" << endl;
+    for (map<int, map<int, int>>::iterator it = FuncCalls.begin(); it != FuncCalls.end(); ++it)
+    {
+        cout << it->first << " calls: ";
+        for (map<int, int>::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2)
+        {
+            cout << it2->first << "(" << it2->second << ") ";
+        }
+        cout << endl;
+    }
+}
+
 void inputCallgraph(vector<string> &callgraph_tokens)
 {
     string token = ""; // to store each line of input
     while (getline(cin, token))
     {                                      // read each line from standard input - stdin
         callgraph_tokens.push_back(token); // add non-empty lines to the vector
+
+        
     }
 }
 
@@ -175,8 +308,14 @@ int main(int argc, char *argv[])
     // and store it in the callgraph_tokens vector
     inputCallgraph(callgraph_tokens);
 
-    // Parse the call graph
-    extract();
+    // Parse the call graph with simple total reduction
+    // and store the function calls in the FuncCalls map
+    // and the function ID to function name mapping in the FunctoID map
+    // and the function name to function ID mapping in the IDtoFunc map
+    // and the maximum function ID in the maxID variable
+    extract_basic_total_reduction();
+
+    extract_total_reduction_weight();
 
 #if DEBUG
     // print the original bitcode
@@ -186,6 +325,9 @@ int main(int argc, char *argv[])
     printFuntionMapping(IDtoFunc);
 
     // print the call functions
-    printCallFunctions(FuncCalls);
+    printCallFunctions(FuncCallTotalReduction);
+
+    // print the call functions with edge weight
+    printCallFunctionsWeight(FuncCallTotalReductionWeight);
 #endif
 }
--- recreate/test_file.cpp
+++ recreate/test_file.cpp
@@ -1,18 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void A() {
   printf("Function A\n");
+  system("echo \"function A calling\"");
+  for (int i = 0; i < 3; i++) {
+    printf("Function A loop %d\n", i);
+  }
+  printf("Function A end\n");
 }
 
 void B() {
   A();
-  B();
+  A();
+  A();
   printf("Function B\n");
 }
 
 void C() {
   A();
-  C();
+  B();
+  A();
+  B();
   printf("Function C\n");
 }
 
